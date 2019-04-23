518d8b586d5e93bfe6d30e091ea762c83a021f9d
Pizaman15 <47151158+Pizaman15@users.noreply.github.com>
2019-04-23 10:45:27 -0400

    finished the classwork

--- game/hero/hero.js
+++ game/hero/hero.js
@@ -1,40 +1,49 @@
-class Person{
-  constructor(image, name, health, location, damage, inventory){
-this._image = "hero";
-this._name = "person";
-this._health = {max: 100, current: undefined};
-this._location = {x: undefined, y: undefined};
-this._damage = {min: 1, max: 10};
+class Hero{
+  constructor(image, name, health, damage){
+this._image = Utils.typeCheck(image, "str", "Person.image");
+this._name = Utils.typeCheck(name, "str", "Person.image");
+this._health = Utils.typeCheck(health, "obj", "Person.health");
+this._location = undefined;
+this._damage = Utils.typeCheck(damage, "obj", "Person.damage")
 this._inventory = [];
   }
 
 take(item){
-inventory.push(item);
+this._inventory.push(item);
 }
 
 drop(item){
-inventory.pull(item);
+  for (var i = 0; i < this._inventory.length; i++) {
+      if(this._inventory[i].name == item){
+          return this._inventory.splice(i)[0];
+    }
+  }
 }
 
 _purge(){
 this._inventory = [];
 }
-  get image(){return image;}
-  set image(image){Utils.typeCheck(image, "str", "person.image");}
+  get image(){ return this._image}
+  set image(image){this._image = Utils.typeCheck(image, "str", "Person.image");}
 
-  get name(){return name;}
-  set name(name){Utils.typeCheck(name, "str", "Person.name")}
+  get name(){ return this._name}
+  set name(name){this._name = Utils.typeCheck(name, "str", "Person.name")}
 
-  get health(){return health;}
-  set health(health){Utils.typeCheck(health, "class", "Person.health")}
+ // if current is greatert than max than set currne tO max
+  get health(){ return this._health}
+  set health(health){
+    health = Utils.typeCheck(health, "obj", "Person.health");
+      if(health.current > health.max){health.current = health.max;}
+      this._health = this.health;
+}
 
-  get location(){return location;}
-  set location(location){utils.typeCheck(location, "obj", "Person.location")}
+  get location(){ return Utils.undefinedCheck(this._location, "Person.location"); }
+  set location(location){this._location = Utils.typeCheck(location, "obj", "Person.location")}
 
-  get damage(){return Utils.undefinedCheck(this._damage, "Dungeon.hero");}// need to have the protection error
-  set damage(damage){Utils.protectionError("Person", "damage");}
+  get damage(){ return Utils.undefinedCheck(this._damage, "Person.damage");}// need to have the protection error
+  set damage(damage){this._damage = Utils.protectionError("Person", "damage");}
 
-  get inventory(){return Utils.undefinedCheck(this._inventory, "Dungeon.hero");}// need to have the protection error
-  set inventory(inventory){Utils.protectionError("Person", "inventory");}
+  get inventory(){ return Utils.undefinedCheck(this._inventory, "Person.inventory");}// need to have the protection error
+  set inventory(inventory){this._inventory = Utils.protectionError("Person", "inventory");}
 
 }
